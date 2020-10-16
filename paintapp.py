89917154cca945d90565381944d25b103652816c
wolframowsky <wolframowsky@gmail.com>
2020-10-16 03:34:28 +0200

    Added realtime cursor position update

--- paintapp.py
+++ paintapp.py
@@ -16,17 +16,19 @@ class Window(QMainWindow):
         self.resize(1200, 800)
         self.setup_ui()
         self.show()
-
+        self.setMouseTracking(True)
     def setup_ui(self):
         # scene setup
         scene = QGraphicsScene(self)
 
         # graphics view setup
         self.graphics_view = GraphicsView(scene, self)
+        self.graphics_view.setup_view()
         self.graphics_view.setGeometry(200, 10, self.graphic_view_width, self.graphic_view_height)
         self.graphics_view.setSceneRect(0, 0, self.graphic_view_width, self.graphic_view_height)
         self.graphics_view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.graphics_view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
         blueBrush = QBrush(Qt.blue)
         blackPen = QPen(Qt.black)
         blackPen.setWidth(5)
@@ -49,6 +51,7 @@ class Window(QMainWindow):
         self.mouse_cord_label.setGeometry(QRect(10, 10, 100, 100))
         self.mouse_cord_label.setText("(x: {}, y: {})".format(self.mouse_x_cord, self.mouse_y_cord))
 
+
     def rotate_minus(self):
         self.graphics_view.rotate(-14)
 
@@ -58,13 +61,15 @@ class Window(QMainWindow):
 
 
 class GraphicsView(QGraphicsView):
+    def setup_view(self):
+        self.setMouseTracking(True)
+
     def mouseMoveEvent(self, event):
-        super(GraphicsView, self).mouseMoveEvent(event)
         mouse_cord = event.pos()
         self.parent().mouse_x_cord = str(mouse_cord.x())
         self.parent().mouse_y_cord = str(mouse_cord.y())
         self.parent().mouse_cord_label.setText("(x: {}, y: {})".format(self.parent().mouse_x_cord, self.parent().mouse_y_cord))
-
+        super(GraphicsView, self).mouseMoveEvent(event)
 
 app = QApplication(sys.argv)
 window = Window()
