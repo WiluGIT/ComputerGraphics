ad5327be03e15d6eb40ba8beceeec41c9b0b982e
wolframowsky <wolframowsky@gmail.com>
2020-10-16 02:54:22 +0200

    Created basic gui events

--- paintapp.py
+++ paintapp.py
@@ -1,14 +1,17 @@
-from PySide2.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QPushButton , QGraphicsView, QGraphicsItem
+from PySide2.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QPushButton, \
+    QGraphicsView, QGraphicsItem, QLabel
 from PySide2.QtGui import QBrush, QPen, QFont
-from PySide2.QtCore import Qt
+from PySide2.QtCore import Qt, QRect, Signal
 import sys
+
 
 class Window(QMainWindow):
     graphic_view_width = 840
     graphic_view_height = 440
+    mouse_x_cord = "0"
+    mouse_y_cord = "0"
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle("PaintApp")
         self.resize(1200, 800)
         self.setup_ui()
@@ -19,7 +22,7 @@ class Window(QMainWindow):
         scene = QGraphicsScene(self)
 
         # graphics view setup
-        self.graphics_view = QGraphicsView(scene, self)
+        self.graphics_view = GraphicsView(scene, self)
         self.graphics_view.setGeometry(200, 10, self.graphic_view_width, self.graphic_view_height)
         self.graphics_view.setSceneRect(0, 0, self.graphic_view_width, self.graphic_view_height)
         self.graphics_view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
@@ -41,15 +44,27 @@ class Window(QMainWindow):
         button2.setGeometry(320, 450, 100, 50)
         button2.clicked.connect(self.rotate_plus)
 
+        # labels
+        self.mouse_cord_label = QLabel(self)
+        self.mouse_cord_label.setGeometry(QRect(10, 10, 100, 100))
+        self.mouse_cord_label.setText("(x: {}, y: {})".format(self.mouse_x_cord, self.mouse_y_cord))
+
     def rotate_minus(self):
-        self.view.rotate(-14)
+        self.graphics_view.rotate(-14)
 
     def rotate_plus(self):
-        self.view.rotate(14)
+        self.graphics_view.rotate(14)
+        print(self.graphics_view.siema)
 
-    def mouseReleaseEvent(QGraphicsSceneMouseEvent):
-        pos = QGrapihcsSceneMouseEvent.lastScenePos()
-        self.signalMousePos.emit(pos)
+
+class GraphicsView(QGraphicsView):
+    def mouseMoveEvent(self, event):
+        super(GraphicsView, self).mouseMoveEvent(event)
+        mouse_cord = event.pos()
+        self.parent().mouse_x_cord = str(mouse_cord.x())
+        self.parent().mouse_y_cord = str(mouse_cord.y())
+        self.parent().mouse_cord_label.setText("(x: {}, y: {})".format(self.parent().mouse_x_cord, self.parent().mouse_y_cord))
+
 
 app = QApplication(sys.argv)
 window = Window()
