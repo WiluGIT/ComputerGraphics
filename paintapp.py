8ef0922b14e2603622de944d2cffe45eb6f04463
wolframowsky <wolframowsky@gmail.com>
2020-10-16 18:17:29 +0200

    Added line drawing

--- paintapp.py
+++ paintapp.py
@@ -1,5 +1,5 @@
 from PySide2.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QPushButton, \
-    QGraphicsView, QGraphicsItem, QLabel
+    QGraphicsView, QGraphicsItem, QLabel, QGraphicsLineItem
 from PySide2.QtGui import QBrush, QPen, QFont
 from PySide2.QtCore import Qt, QRect, QPoint
 import sys
@@ -21,25 +21,12 @@ class Window(QMainWindow):
         self.setup_ui()
         self.show()
         self.setMouseTracking(True)
+
     def setup_ui(self):
-        # scene setup
-        scene = QGraphicsScene(self)
-
         # graphics view setup
-        self.graphics_view = GraphicsView(scene, self)
-        self.graphics_view.setup_view()
+        self.graphics_view = GraphicsView(self)
         self.graphics_view.setGeometry(200, 10, self.graphic_view_width, self.graphic_view_height)
-        self.graphics_view.setSceneRect(0, 0, self.graphic_view_width, self.graphic_view_height)
-        self.graphics_view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.graphics_view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
-        blueBrush = QBrush(Qt.blue)
-        blackPen = QPen(Qt.black)
-        blackPen.setWidth(5)
-        ellipse = scene.addEllipse(10, 10, 200, 200, blackPen)
-        rect = scene.addRect(0, 0, 200, 200, blackPen, blueBrush)
-        ellipse.setFlag(QGraphicsItem.ItemIsMovable)
-        rect.setFlag(QGraphicsItem.ItemIsMovable)
 
         # buttons
         button = QPushButton("Rotate - ", self)
@@ -63,13 +50,34 @@ class Window(QMainWindow):
 
 
 class GraphicsView(QGraphicsView):
-    def setup_view(self):
+    siema = "sdadsadasd"
+    def __init__(self, parent=None):
+        super(GraphicsView, self).__init__(parent)
+        self.setup_ui()
+
+        blueBrush = QBrush(Qt.blue)
+        blackPen = QPen(Qt.black)
+        blackPen.setWidth(5)
+        ellipse = self.scene.addEllipse(10, 10, 200, 200, blackPen)
+        rect = self.scene.addRect(0, 0, 200, 200, blackPen, blueBrush)
+        line = self.scene.addLine(10, 300, 300, 350, blackPen)
+        ellipse.setFlag(QGraphicsItem.ItemIsMovable)
+        rect.setFlag(QGraphicsItem.ItemIsMovable)
+
+    def setup_ui(self):
+        # scene setup
+        self.scene = QGraphicsScene(self)
+        self.setScene(self.scene)
+        self.scene.setSceneRect(0, 0, self.parent().graphic_view_width, self.parent().graphic_view_height)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.setMouseTracking(True)
 
     def mouseMoveEvent(self, event):
         mouse_cord = event.pos()
         self.parent().mouse_cords = mouse_cord
-        self.parent().mouse_cord_label.setText("(x: {}, y: {})".format(self.parent().mouse_cords.x(), self.parent().mouse_cords.y()))
+        self.parent().mouse_cord_label.setText(
+            "(x: {}, y: {})".format(self.parent().mouse_cords.x(), self.parent().mouse_cords.y()))
         super(GraphicsView, self).mouseMoveEvent(event)
 
     def mousePressEvent(self, event):
@@ -81,8 +89,23 @@ class GraphicsView(QGraphicsView):
     def mouseReleaseEvent(self, event):
         mouse_cord = event.pos()
         self.parent().end_point_cords = mouse_cord
-        print(self.parent().end_point_cords)
+        self.draw()
         super(GraphicsView, self).mouseReleaseEvent(event)
+
+    def draw(self):
+        print(self.scene)
+        Line(self.scene, self.parent().start_point_cords, self.parent().end_point_cords)
+
+
+class Line(QGraphicsItem):
+    def __init__(self, scene, start_cord, end_cord):
+        blackPen = QPen(Qt.black)
+        blackPen.setWidth(10)
+        self.line = QGraphicsLineItem(start_cord.x(), start_cord.y(), end_cord.x(), end_cord.y())
+        self.line.setPen(blackPen)
+        self.line.setFlag(QGraphicsItem.ItemIsMovable)
+        scene.addItem(self.line)
+
 
 
 
