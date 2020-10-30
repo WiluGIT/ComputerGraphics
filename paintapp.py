96b0966c0eb037ecc7ff6053c75ad0a33f4ddb08
wolframowsky <wolframowsky@gmail.com>
2020-10-30 23:37:34 +0100

    Added opengl rgb cube

--- paintapp.py
+++ paintapp.py
@@ -3,17 +3,20 @@ import os
 from enum import Enum
 
 import numpy as np
+from OpenGL.arrays import vbo
 from PIL import Image
 import cv2
-#from scipy.misc
+from PySide2.QtOpenGL import QGLWidget
 from PySide2.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QPushButton, \
     QGraphicsView, QGraphicsItem, QLabel, QGraphicsLineItem, QGraphicsRectItem, QGraphicsEllipseItem, QGraphicsObject, \
     QButtonGroup, QLineEdit, QLayout, QMessageBox, QComboBox, QStyle, QMenuBar, QMenu, QAction, QStatusBar, QFileDialog, \
-    QGraphicsPixmapItem, QDialogButtonBox, QSlider, QDialog, QSpinBox, QAbstractSlider
+    QGraphicsPixmapItem, QDialogButtonBox, QSlider, QDialog, QSpinBox, QAbstractSlider, QVBoxLayout, QWidget
 from PySide2.QtGui import QBrush, QPen, QFont, QPainter, QColor, QRegExpValidator, QIcon, QPixmap, QImage
 from PySide2.QtCore import Qt, QRect, QPoint, Signal, QPointF, QRectF, Slot, QLineF, QSize
-from PySide2 import QtCore
+from PySide2 import QtCore, QtOpenGL
 import sys
+import OpenGL.GL as gl
+from OpenGL import GLU
 
 
 class ToolSelect(Enum):
@@ -119,6 +122,10 @@ class Window(QMainWindow):
         self.set_color_button = QPushButton("Select color", self)
         self.set_color_button.setGeometry(QtCore.QRect(70, 180, 80, 20))
         self.set_color_button.clicked.connect(self.select_color_window)
+
+        self.cube_button = QPushButton("Cube", self)
+        self.cube_button.setGeometry(QtCore.QRect(70, 200, 80, 20))
+        self.cube_button.clicked.connect(self.show_cube)
 
         # labels
         self.photo = QLabel(self)
@@ -245,6 +252,32 @@ class Window(QMainWindow):
         # sections visibility
         self.setTextCreatorSectionVisibility(False)
         self.setResizerVisabilitySection(False)
+
+    def show_cube(self):
+        self.window = CubeWindow()
+        self.window.show()
+        # self.window = QGLWidget()
+        # self.glWidget = GLWidget(self.window)
+        #
+        # self.central_widget = QWidget()
+        # self.guiLayout = QVBoxLayout()
+        # self.central_widget.setLayout(self.guiLayout)
+        # self.guiLayout.addWidget(self.glWidget)
+        #
+        # sliderX = QSlider(QtCore.Qt.Horizontal)
+        # sliderX.valueChanged.connect(lambda val: self.glWidget.setRotX(val))
+        #
+        # sliderY = QSlider(QtCore.Qt.Horizontal)
+        # sliderY.valueChanged.connect(lambda val: self.glWidget.setRotY(val))
+        #
+        # sliderZ = QSlider(QtCore.Qt.Horizontal)
+        # sliderZ.valueChanged.connect(lambda val: self.glWidget.setRotZ(val))
+        #
+        # self.gui_layout.addWidget(sliderX)
+        # self.gui_layout.addWidget(sliderY)
+        # self.gui_layout.addWidget(sliderZ)
+
+        #self.window.show()
 
     def select_color_window(self):
         self.window = QDialog()
@@ -919,13 +952,6 @@ class Color_Dialog(object):
         self.cancel.setGeometry(460, 240, 50, 30)
         self.ok.clicked.connect(Dialog.accept)
         self.cancel.clicked.connect(Dialog.reject)
-        # self.buttonBox.accepted.connect(Dialog.accept)
-        # self.buttonBox.rejected.connect(Dialog.reject)
-        # self.buttonBox = QDialogButtonBox(Dialog)
-        # self.buttonBox.setGeometry(QtCore.QRect(280, 240, 171, 32))
-        # self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        # self.buttonBox.setStandardButtons(cancel|ok)
-        # self.buttonBox.setObjectName("buttonBox")
 
         self.cSlider = QSlider(Dialog)
         self.cSlider.setGeometry(QtCore.QRect(50, 20, 160, 22))
@@ -1154,6 +1180,128 @@ class Color_Dialog(object):
             self.color_value_label.setStyleSheet(color_label)
         except ZeroDivisionError:
             pass
+
+class CubeWindow(QWidget):
+    def __init__(self):
+        super(CubeWindow, self).__init__()
+        self.resize(400, 600)
+        self.glWidget = GLWidget()
+        self.guiLayout = QVBoxLayout()
+
+        self.guiLayout.addWidget(self.glWidget)
+
+        sliderX = QSlider(QtCore.Qt.Horizontal)
+        sliderX.valueChanged.connect(lambda val: self.glWidget.setRotX(val))
+
+        sliderY = QSlider(QtCore.Qt.Horizontal)
+        sliderY.valueChanged.connect(lambda val: self.glWidget.setRotY(val))
+
+        sliderZ = QSlider(QtCore.Qt.Horizontal)
+        sliderZ.valueChanged.connect(lambda val: self.glWidget.setRotZ(val))
+
+        self.guiLayout.addWidget(sliderX)
+        self.guiLayout.addWidget(sliderY)
+        self.guiLayout.addWidget(sliderZ)
+        self.setLayout(self.guiLayout)
+        timer = QtCore.QTimer(self)
+        timer.setInterval(20)   # period, in milliseconds
+        timer.timeout.connect(self.glWidget.updateGL)
+        timer.start()
+
+
+class GLWidget(QGLWidget):
+    def __init__(self, parent=None):
+        self.parent = parent
+        QtOpenGL.QGLWidget.__init__(self, parent)
+
+    def initializeGL(self):
+        self.qglClearColor(QColor(0, 0, 255))  # initialize the screen to blue
+        gl.glEnable(gl.GL_DEPTH_TEST)  # enable depth testing
+
+        self.initGeometry()
+
+        self.rotX = 0.0
+        self.rotY = 0.0
+        self.rotZ = 0.0
+
+    def resizeGL(self, width, height):
+        gl.glViewport(0, 0, width, height)
+        gl.glMatrixMode(gl.GL_PROJECTION)
+        gl.glLoadIdentity()
+        aspect = width / float(height)
+
+        GLU.gluPerspective(45.0, aspect, 1.0, 100.0)
+        gl.glMatrixMode(gl.GL_MODELVIEW)
+
+    def paintGL(self):
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+
+        gl.glPushMatrix()  # push the current matrix to the current stack
+
+        gl.glTranslate(0.0, 0.0, -50.0)  # third, translate cube to specified depth
+        gl.glScale(20.0, 20.0, 20.0)  # second, scale cube
+        gl.glRotate(self.rotX, 1.0, 0.0, 0.0)
+        gl.glRotate(self.rotY, 0.0, 1.0, 0.0)
+        gl.glRotate(self.rotZ, 0.0, 0.0, 1.0)
+        gl.glTranslate(-0.5, -0.5, -0.5)  # first, translate cube center to origin
+
+        gl.glEnableClientState(gl.GL_VERTEX_ARRAY)
+        gl.glEnableClientState(gl.GL_COLOR_ARRAY)
+
+        gl.glVertexPointer(3, gl.GL_FLOAT, 0, self.vertVBO)
+        gl.glColorPointer(3, gl.GL_FLOAT, 0, self.colorVBO)
+
+        gl.glDrawElements(gl.GL_QUADS, len(self.cubeIdxArray), gl.GL_UNSIGNED_INT, self.cubeIdxArray)
+
+        gl.glDisableClientState(gl.GL_VERTEX_ARRAY)
+        gl.glDisableClientState(gl.GL_COLOR_ARRAY)
+
+        gl.glPopMatrix()  # restore the previous modelview matrix
+
+    def initGeometry(self):
+        self.cubeVtxArray = np.array(
+            [[0.0, 0.0, 0.0],
+             [1.0, 0.0, 0.0],
+             [1.0, 1.0, 0.0],
+             [0.0, 1.0, 0.0],
+             [0.0, 0.0, 1.0],
+             [1.0, 0.0, 1.0],
+             [1.0, 1.0, 1.0],
+             [0.0, 1.0, 1.0]])
+        self.vertVBO = vbo.VBO(np.reshape(self.cubeVtxArray,
+                                          (1, -1)).astype(np.float32))
+        self.vertVBO.bind()
+
+        self.cubeClrArray = np.array(
+            [[0.0, 0.0, 0.0],
+             [1.0, 0.0, 0.0],
+             [1.0, 1.0, 0.0],
+             [0.0, 1.0, 0.0],
+             [0.0, 0.0, 1.0],
+             [1.0, 0.0, 1.0],
+             [1.0, 1.0, 1.0],
+             [0.0, 1.0, 1.0]])
+        self.colorVBO = vbo.VBO(np.reshape(self.cubeClrArray,
+                                           (1, -1)).astype(np.float32))
+        self.colorVBO.bind()
+
+        self.cubeIdxArray = np.array(
+            [0, 1, 2, 3,
+             3, 2, 6, 7,
+             1, 0, 4, 5,
+             2, 1, 5, 6,
+             0, 3, 7, 4,
+             7, 6, 5, 4])
+
+    def setRotX(self, val):
+        self.rotX = np.pi * val
+
+
+    def setRotY(self, val):
+        self.rotY = np.pi * val
+
+    def setRotZ(self, val):
+        self.rotZ = np.pi * val
 
 
 app = QApplication(sys.argv)
